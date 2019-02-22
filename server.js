36fa9d21b14f96f73314af134b9e3fadbc69cbad
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-22 15:56:17 -0800

    Set up views folder, index page from root route.

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-})
+});
